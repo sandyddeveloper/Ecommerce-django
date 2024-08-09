237da53bcf46cac996e6e1505a356d681ac71b6a
sandyddeveloper <sandyddeveloper@gmail.com>
2024-08-09 21:15:13 +0530

    login added

--- Trendify/authentication/views.py
+++ Trendify/authentication/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def signup (request):
     return render(request, 'auth/signup.html')
+
+def handlelogin (request):
+    return render(request, 'auth/login.html')
