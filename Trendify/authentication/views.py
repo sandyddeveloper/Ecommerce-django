9b0b2b79180e94af9517b25acadc4d113550be6d
sandyddeveloper <sandyddeveloper@gmail.com>
2024-08-09 22:37:49 +0530

    sign working..

--- Trendify/authentication/views.py
+++ Trendify/authentication/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
 
 # Create your views here.
-def signup (request):
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        firstName = request.POST.get('firstName')
+        lastName = request.POST.get('lastName')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        confirmPassword = request.POST.get('confirmPassword')
+        
+        if password != confirmPassword:
+            messages.warning(request, "Passwords do not match")
+            return render(request, 'auth/signup.html')
+        try:
+            if User.objects.get(email=email):
+                messages.warning(request, "User already exists")
+                return render(request, 'auth/signup.html')
+        except User.DoesNotExist:
+            pass
+        
+        myuser = User.objects.create_user(username, email, password)
+        myuser.first_name = firstName
+        myuser.last_name = lastName
+        myuser.save()
+        messages.success(request, "Account created successfully")
+        return redirect('/auth/login')  # Make sure this URL matches your URL patterns
+    
     return render(request, 'auth/signup.html')
-
 def handlelogin (request):
     return render(request, 'auth/login.html')
